5c6f8bcb80f7225a2f70eb48b730628c24c31ac2
postscriptumno <postscriptum.no@gmail.com>
2020-08-15 11:51:07 +0300

    Заменил создание шаблона на создание соответствующих классов

--- src/view/task-list.js
+++ src/view/task-list.js
@@ -2,19 +2,17 @@ import {
   createElement
 } from "../utils.js";
 
-const createBoardTemplate = () => {
-  return (
-    `<section class="board container"></section>`
-  );
+const createTaskListTemplate = () => {
+  return `<div class="board__tasks"></div>`;
 };
 
-export default class Board {
+export default class TaskList {
   constructor() {
     this._element = null;
   }
 
   getTemplate() {
-    return createBoardTemplate();
+    return createTaskListTemplate();
   }
 
   getElement() {
--- src/view/task.js
+++ src/view/task.js
@@ -1,6 +1,11 @@
-import {isTaskExpired, isTaskRepeating, humanizeTaskDueDate} from "../utils.js";
+import {
+  isTaskExpired,
+  isTaskRepeating,
+  humanizeTaskDueDate,
+  createElement
+} from "../utils.js";
 
-export const createTaskTemplate = (task) => {
+const createTaskTemplate = (task) => {
   const {
     color,
     description,
@@ -64,3 +69,27 @@ export const createTaskTemplate = (task) => {
 	</article>`
   );
 };
+
+export default class Task {
+  constructor(task) {
+    this._task = task;
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTaskTemplate(this._task);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
