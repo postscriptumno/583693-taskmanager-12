204d70363fdab6acb4aa24ce23d94e3288637712
postscriptumno <postscriptum.no@gmail.com>
2020-08-17 13:58:22 +0300

    Created an abstract class and inherited other views from it. Moved event handlers into classes methods

--- src/view/load-more-button.js
+++ src/view/load-more-button.js
@@ -1,6 +1,4 @@
-import {
-  createElement
-} from "../utils.js";
+import AbstractView from "./abstract.js";
 
 const createLoadMoreButtonTemplate = () => {
   return (
@@ -8,24 +6,24 @@ const createLoadMoreButtonTemplate = () => {
   );
 };
 
-export default class TaskList {
+export default class TaskList extends AbstractView {
   constructor() {
-    this._element = null;
+    super();
+
+    this._clickHandler = this._clickHandler.bind(this);
   }
 
   getTemplate() {
     return createLoadMoreButtonTemplate();
   }
 
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
+  _clickHandler(evt) {
+    evt.preventDefault();
+    this._callback.click();
   }
 
-  removeElement() {
-    this._element = null;
+  setClickHandler(callback) {
+    this._callback.click = callback;
+    this.getElement().addEventListener(`click`, this._clickHandler);
   }
 }
